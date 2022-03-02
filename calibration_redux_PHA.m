22005c50101c85042b271ef8adbb32dfb0c5dd8c
ceschi <emanuele.franceschi@gmail.com>
2022-03-02 11:49:33 +0100

    plots en format eps

--- calibration_redux_PHA.m
+++ calibration_redux_PHA.m
@@ -74,7 +74,7 @@ legend('Sans PT', 'dA=1%', 'dA=1.8%', 'dA=1.4%', 'dA=0%', 'dA=.25%', ...
      'Location', 'best');
 title('PIB');
 saveas(pibs, './plots/redux_PHA_pib.eps', 'epsc');
-% exportgraphics(pibs,'./plots/redux_PHA_pibs.jpg','Resolution',600);
+exportgraphics(pibs,'./plots/redux_PHA_pibs.jpg','Resolution',600);
 
 pts_macro = figure(2);
 subplot(1, 2, 1);
@@ -98,7 +98,7 @@ legend('Sans PT', 'dA=1%', 'dA=1.8%', 'dA=1.4%', 'dA=0%', 'dA=.25%', ...
      'Location', 'best');
 title('Productivité agregée des qualifiés')
 saveas(pts_macro, './plots/redux_PHA_TFPs.eps', 'epsc');
-% exportgraphics(pts_macro,'./plots/redux_PHA_TFPs.jpg','Resolution',600);
+exportgraphics(pts_macro,'./plots/redux_PHA_TFPs.jpg','Resolution',600);
 
 pts_micro = figure(3);
 plot(1:9, .7 + log(1:9)/10, '-', ...
@@ -106,4 +106,4 @@ plot(1:9, .7 + log(1:9)/10, '-', ...
 legend('NQ', 'Q', 'location', 'best');
 title('Profils de productivités individuelles');
 saveas(pts_micro, './plots/redux_PHA_prodmicro.eps', 'epsc');
-% exportgraphics(pts_micro,'./plots/redux_PHA_prodmicro.jpg','Resolution',600)
+exportgraphics(pts_micro,'./plots/redux_PHA_prodmicro.jpg','Resolution',600)
